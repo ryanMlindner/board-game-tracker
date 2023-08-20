e5a0300a0858281d0858bada9bb886eabf587a9d
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-08-20 17:00:37 -0600

    routes, todo components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 
 //client todos! hell yeah! TODO login,signup,logout first?
-function App() {
-  return <h1>Phase 4 Project Client</h1>;
+//imports --home.js
+export default function App() {
+  return (
+    <div className="ui full-page">
+			
+      <NavBar />
+      
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/signup">
+          <Signup/>
+        </Route>
+        <Route exact path="/login">
+          <Login/>
+        </Route>
+        <Route exact path="/allscores">
+          <AllScores/>
+        </Route>
+      </Switch>
+    </div>
+  )
 }
-
-export default App;
+//TODO more routes as needed
