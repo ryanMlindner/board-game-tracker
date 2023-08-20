4f249b07198a5046393efe3eac1b2b01f26ec8c8
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-08-20 16:55:35 -0600

    client init

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
+//client todos! hell yeah! TODO login,signup,logout first?
 function App() {
   return <h1>Phase 4 Project Client</h1>;
 }
