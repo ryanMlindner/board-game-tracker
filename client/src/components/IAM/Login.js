8c5cfd8d5524f6c08799a85870dba2ae91090fae
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-08-21 23:14:03 -0600

    login untested

--- client/src/components/IAM/Login.js
+++ client/src/components/IAM/Login.js
@@ -1,8 +1,48 @@
 import React from "react";
+import { useSetRecoilState } from "recoil";
+import { userAtom } from "../HelperFunctions/atoms";
 //TODO all this stuff
 export default function Login() {
+  const setUser = useSetRecoilState(userAtom)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }
   return (
     <div className="ui full-page">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+        />
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
   )
 }
