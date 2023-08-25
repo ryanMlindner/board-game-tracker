a7917e2c814d256ee35ffab072f593d1a28b18d3
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-08-25 01:50:35 -0600

    landing page update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,18 +8,31 @@ export default function Home() {
     <div className="ui full-page">
       <div className="ui hidden divider"></div>
       {user ? 
-      <h2>
-      Welcome, {user.username}
-      </h2>
+      <div className="ui segment">
+        <h2>Welcome, {user.username}</h2>
+      </div>
       : <h4>Sign up or Log in PLACEHOLDER</h4>
       }
       <h1>How To:</h1>
-      <p>Login or Signup, then add <b>New Players</b> and add <b>Board Games</b> as you see fit. 
+      <div className="ui large segment">
+        Login or Signup, then add <b>New Players</b> and 
+        add <b>New Board Games</b> as you see fit. 
         Add <b>New Sessions</b> for nights of games, then add the <b>New Played Games</b> that 
         session using the dropdowns provided in those inputs. After adding a game for a session, 
         add in the <b>New Scores</b> for each player that participated. The <b>Totals</b> page will 
         update with statistics for each player as soon as the first score is submitted.
-      </p>
+      </div>
+      <h4>About the Author:</h4>
+      <div className="ui tertiary segment">
+        Ryan Lindner is a software engineer who loves to play board games in his spare time,
+        and is pretty terrible at thinking of things to say about himself.
+        Perhaps there will be more information here someday, but for now have some links.
+      </div>
+      <div className="ui grid">
+        <a className="five wide column" href="https://github.com/ryanMlindner">Github</a>
+        <a className="five wide column" href="https://www.linkedin.com/in/ryan-lindner-5697b1264/">LinkedIn</a>
+        <a className="five wide column" href="https://medium.com/@ryan.m.lindner">Blog</a>
+      </div>
     </div>
   )
 }
