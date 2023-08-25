83726e7904a5a20a2ffa5ad419cf987ae424d160
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-08-25 01:15:58 -0600

    update seed.py

--- server/seed.py
+++ server/seed.py
@@ -23,48 +23,8 @@ if __name__ == '__main__':
         Score.query.delete()
         User.query.delete()
         print("Starting seed...")
-        #test code comment out eventually
         print("setting up admin")
         admin = User(username="admin")
         admin.password_hash = "bananas"
         db.session.add(admin)
-        db.session.commit()
-        #TODO test code is broken anyways because of user
-        game = Game(
-            title="Definitely Game",
-            publisher="publisher of Game",
-            genre="totally a genre",
-        )
-        db.session.add(game)
-        db.session.commit()
-        player = Player(
-            name="Bob",
-        )
-        db.session.add(player)
-        db.session.commit()
-        session = Session(
-            date = '08/24/2023',
-            user_id = 1
-        )
-        db.session.add(session)
-        db.session.commit()
-        attendance = Attendance(
-            player_id = 1,
-            session_id = 1
-        )
-        db.session.add(attendance)
-        db.session.commit()
-        gameinstance = GameInstance(
-            session_id = 1,
-            game_id = 1
-        )
-        db.session.add(gameinstance)
-        db.session.commit()
-        score = Score(
-            points=10,
-            placement=1,
-            player_id=1,
-            game_instance_id=1,
-        )
-        db.session.add(score)
         db.session.commit()
