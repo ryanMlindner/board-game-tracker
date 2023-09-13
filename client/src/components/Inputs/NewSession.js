da124bcbab2a10fc382e218d5a1ba78386c1426f
RyanLindner <ryan.m.lindner@gmail.com>
2023-09-12 19:20:54 -0600

    array of players to add to attendance, css on player buttons(placeholder)

--- client/src/components/Inputs/NewSession.js
+++ client/src/components/Inputs/NewSession.js
@@ -44,35 +44,53 @@ export default function NewSession() {
     })
   }
 
+  function addToSession(player) {
+    playersInSession.push(player);
+    console.log(playersInSession);
+  }
+
+  function removeFromSession(player) {
+    playersInSession.pop(player);
+    console.log(playersInSession);
+  }
+
   return (
     <div className="full-page">
       <div className="ui hidden divider"></div>
       {user ?
       <div>
-        <form className="ui form" onSubmit={handleSubmit}>
+        <form className="" onSubmit={handleSubmit}>
           <h1>New Sesh</h1>
-          <label htmlFor="date">Date</label>
-          <input
-            type="text"
-            id="date"
-            autoComplete="off"
-            value={date}
-            placeholder="mm/dd/yyyy"
-            onChange={(e) => setDate(e.target.value)}
-          />
-          <div className="container">
-            <div className="row">
+          <div className="row">
+            <div className="6 columns">
+              <label htmlFor="date">Date</label>
+              <input
+                type="text"
+                id="date"
+                autoComplete="off"
+                value={date}
+                placeholder="mm/dd/yyyy"
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </div>
+          </div>
+          <button className="button-primary" type="submit">Add Session</button>
+        </form>
+        <div className="container">
+          <div className="row">
             {players ?
             players.map(player => {
-              return <PlayerButton key = {player.id} player = {player}/>
+              return <PlayerButton 
+              key = {player.id} 
+              player = {player} 
+              addToSession={addToSession}
+              removeFromSession={removeFromSession}/>
             })
             :
             <h3>No Players added for this user, Add players first!</h3>
             }
-            </div>
           </div>
-          <button className="ui primary button" type="submit">Add Session</button>
-        </form>
+        </div>
       <div className="ui hidden divider"></div>
       {session ?
       <div>Last session added: {session.date}</div>
--- client/src/components/Inputs/PlayerButton.js
+++ client/src/components/Inputs/PlayerButton.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 
-export default function PlayerButton( { player } ) {
+export default function PlayerButton( { player, addToSession, removeFromSession } ) {
     const [inSession, setInSession] = useState(false);
 
     function handleClick() {
+        if (!inSession){addToSession(player)}
+        else {removeFromSession(player)}
         setInSession(!inSession)
     }
 
     return (
-        <div className="three columns">
-            <button className="button" onClick={handleClick}>{player.name}</button>
+        <div className="two columns">
+            <button className={inSession ? "button-primary" : "button"}
+             onClick={handleClick}>{player.name}</button>
         </div>
     )
 }
